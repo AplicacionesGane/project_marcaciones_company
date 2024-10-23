4b470623d35e3b1ace96255b8a81ab54ac03b9c0
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-23 14:20:59 -0500

    change properties for model Persona

--- server/src/models/persona.model.ts
+++ server/src/models/persona.model.ts
@@ -1,51 +1,31 @@
+import { Model, InferAttributes, InferCreationAttributes, DataTypes } from 'sequelize';
 import { GrupoTurnoVsHorario } from './gpTurnoVsHorario.model';
-import { Model, DataTypes, Optional } from 'sequelize';
 import { db_connection } from '../connections';
 import { Area } from './areas.model';
 
-interface PersonaAttributes {
-  id: number;
-  identificacion: string;
-  nombres: string;
-  apellidos: string;
-  id_Empresa: number;
-  estado: string;
-  id_Grupo_Horario: number | null;
-  id_Areas: number;
-  id_Ciudad: number;
-  id_Centro_Costos: number;
-  id_Cargo: number;
-}
-
-type PersonaCreationAttributes = Optional<PersonaAttributes, 'id'>;
-
-class Persona extends Model<PersonaAttributes, PersonaCreationAttributes> implements PersonaAttributes {
-  declare id: number;
-  declare identificacion: string;
-  declare nombres: string;
-  declare apellidos: string;
-  declare id_Empresa: number;
-  declare estado: string;
-  declare id_Grupo_Horario: number | null;
-  declare id_Areas: number;
-  declare id_Ciudad: number;
-  declare id_Centro_Costos: number;
-  declare id_Cargo: number;
-  declare GrupoTurnoVsHorarios: GrupoTurnoVsHorario[];
-  declare Area: Area | null;
+class Persona extends Model<InferAttributes<Persona>, InferCreationAttributes<Persona>> {
+  declare id: number
+  declare tipoIdenticicacion: string
+  declare identificacion: string
+  declare nombres: string
+  declare apellidos: string
+  declare estado: 'A' | 'R'
+  declare id_Grupo_Horario: number | null
+  declare id_Areas: number | null
+  declare id_Ciudad: number | null
+  declare id_Cargo: number | null
 }
 
 Persona.init({
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, },
-  identificacion: { type: DataTypes.STRING, allowNull: false },
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
+  tipoIdenticicacion: { type: DataTypes.STRING(50), allowNull: true},
+  identificacion: { type: DataTypes.STRING(50), allowNull: false },
   nombres: { type: DataTypes.STRING, allowNull: false },
   apellidos: { type: DataTypes.STRING, allowNull: false },
-  id_Empresa: { type: DataTypes.INTEGER, allowNull: false },
   estado: { type: DataTypes.STRING, allowNull: false },
   id_Grupo_Horario: { type: DataTypes.INTEGER, allowNull: true },
   id_Areas: { type: DataTypes.INTEGER, allowNull: false },
   id_Ciudad: { type: DataTypes.INTEGER, allowNull: false },
-  id_Centro_Costos: { type: DataTypes.INTEGER, allowNull: false },
   id_Cargo: { type: DataTypes.INTEGER, allowNull: false },
 
 }, {
