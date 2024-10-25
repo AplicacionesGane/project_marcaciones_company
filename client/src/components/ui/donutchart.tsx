c8b2254e99b6c3bbd7f81a49c31deaccd91890c6
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-10-25 16:37:34 -0500

    graficas

--- client/src/components/ui/donutchart.tsx
+++ client/src/components/ui/donutchart.tsx
@@ -1,13 +1,11 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PieChart } from '@mui/x-charts/PieChart';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function BasicPie({ datos }: { datos: any }) {
   console.log('datos', datos)
 
-  const colores = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'];
+  const colores = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#e414fd', '#fd1429'];
 
-  // Asegúrese de que datos.stados existe antes de usarlo
   const estadosData = datos.stados ? Object.entries(datos.stados).map(([label, value], index) => ({
     id: label,
     value: value as number,
@@ -15,12 +13,55 @@ export function BasicPie({ datos }: { datos: any }) {
     color: colores[index % colores.length]
   })) : [];
 
+  const estadosDatas = [{
+    value: datos.count,
+    label: 'cantidad de marcaciones',
+    color: colores[4]  
+  },
+
+  {
+    value: datos.totalPersona,
+    label: 'personar registradas',
+    color: colores[5]  
+  }
+];
+
 
   return (
-    <PieChart
-      series={[
-        {
-          data: estadosData,
+    <>
+      <PieChart
+        series={[
+          {
+            data: estadosData,
+            highlightScope: { fade: 'global', highlight: 'item' },
+            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+            innerRadius: 40,
+            outerRadius: 130,
+            paddingAngle: 2,
+            cornerRadius: 5,
+            startAngle: -90,
+            endAngle: 270,
+            cx: 150,
+            cy: 150,
+          },
+        ]}
+        width={400}
+        height={400}
+        slotProps={{
+          legend: {
+            direction: 'row',
+            position: { vertical: 'bottom', horizontal: 'left' },
+            padding: 20,
+            labelStyle: {
+              fontSize: 17,
+              textAlign: 'left'
+            },
+          },
+        }} />
+
+      <PieChart
+        series={[{
+          data: estadosDatas,
           highlightScope: { fade: 'global', highlight: 'item' },
           faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
           innerRadius: 40,
@@ -31,21 +72,21 @@ export function BasicPie({ datos }: { datos: any }) {
           endAngle: 270,
           cx: 150,
           cy: 150,
-        },
-      ]}
-      width={400}
-      height={400}
-      slotProps={{
-        legend: {
-          direction: 'row',
-          position: { vertical: 'bottom', horizontal: 'left' },
-          padding: 20,
-          labelStyle: {
-            fontSize: 17,
-            textAlign: 'left'
+        }]}
+        width={400}
+        height={400}
+        slotProps={{
+          legend: {
+            direction: 'row',
+            position: { vertical: 'bottom', horizontal: 'left' },
+            padding: 20,
+            labelStyle: {
+              fontSize: 17,
+              textAlign: 'left',
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+    </>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import { useNavigate } from 'react-router-dom'
 import { BasicPie } from '../components/ui/donutchart'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { URL_API } from '../utils/contants'
 
 export default function Home() {
-  const navigate = useNavigate()
 
   const [data, setData] = useState()
   useEffect(() => {
@@ -26,15 +24,6 @@ export default function Home() {
 
     <main className='h-[90vh] 5cont_main flex flex-row items-center justify-center dark:text-white'>
        <BasicPie datos={data || {}}></BasicPie>
-      <article className='flex flex-col items-center justify-center'>
-      <h1 className='text-5xl font-bold mb-4 animate-bounce'>Welcome to Our Website!</h1>
-      <p className='text-lg mb-8 text-center max-w-md'>
-        Aplicativo en desarrollo para la gestión de empleados y sus marcaciones. generación de reportes y auditorias.
-      </p>
-      <button onClick={() => navigate('/empleados')} className='px-6 py-3 bg-white text-blue-500 font-semibold rounded-lg shadow-md hover:bg-gray-100 transition duration-300 transform hover:scale-105'>
-        Get Started
-      </button>
-      </article>
     </main>
   )
 }
