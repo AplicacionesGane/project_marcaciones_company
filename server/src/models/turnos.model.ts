1045a2fd950dd0f352806d1ecf07c78948836c78
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-07 12:09:24 -0500

    Refactor Turnos model to use InferAttributes and InferCreationAttributes for improved type inference

--- server/src/models/turnos.model.ts
+++ server/src/models/turnos.model.ts
@@ -1,22 +1,8 @@
-import { DataTypes, Model, Optional } from "sequelize";
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize';
 import { db_connection } from '../connections';
 
-interface TurnoAttributes {
-  id: number;
-  codigo: string;
-  descripcion: string;
-  hora_inicio: string;
-  hora_fin: string;
-  teorico: string;
-  hora_inicio_break: string;
-  hora_fin_break: string;
-  tiempo_breack: string;
-  conceptos: string;
-}
 
-type TurnoCreationAttributes = Optional<TurnoAttributes, "id" | 'hora_inicio_break' | 'hora_fin_break' | 'tiempo_breack'>;
-
-export class Turnos extends Model<TurnoAttributes, TurnoCreationAttributes> implements TurnoCreationAttributes {
+export class Turnos extends Model<InferAttributes<Turnos>, InferCreationAttributes<Turnos>> {
   declare id: number;
   declare codigo: string;
   declare descripcion: string;
@@ -43,7 +29,7 @@ Turnos.init(
     conceptos: { type: DataTypes.STRING, allowNull: true },
   },
   {
-    tableName: "turnotiempos",
+    tableName: 'turnotiempos',
     sequelize: db_connection,
     timestamps: false,
   }
